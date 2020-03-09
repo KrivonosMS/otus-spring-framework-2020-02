c923bda6bb00606cc76708bbf1c7456b3b55d21a
KrivonosMS <MS.Krivonos@gamil.com>
2020-03-09 19:40:40 +0300

    fix(homework-02): вернул вывод вопросов в app-сервис

--- homework-02/src/main/java/ru/otus/krivonos/exam/application/ApplicationService.java
+++ homework-02/src/main/java/ru/otus/krivonos/exam/application/ApplicationService.java
@@ -38,6 +38,7 @@ public class ApplicationService {
 			Map<QuestionNumber, Question> questions = checkList.unmodifiableQuestions();
 			Map<QuestionNumber, String> answers = new HashMap<>();
 			for (QuestionNumber questionNumber : questions.keySet()) {
+				System.out.println(questions.get(questionNumber).question());
 				answers.put(questionNumber, scanReader.nextLine());
 			}
 			PersonAnswers personAnswers = PersonAnswers.createInstanceFrom(answers, username);
