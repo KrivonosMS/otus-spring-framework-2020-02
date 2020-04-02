b3dc736e9d9819f8171f774273a8385dabe93837
KrivonosMS <MS.Krivonos@gamil.com>
2020-04-02 19:44:29 +0300

    test(homework-05): добавлнены тестовые методы для shell

--- homework-05/src/test/java/ru/otus/krivonos/library/shell/ShellControllerTest.java
+++ homework-05/src/test/java/ru/otus/krivonos/library/shell/ShellControllerTest.java
@@ -1,14 +1,16 @@
 package ru.otus.krivonos.library.shell;
 
-import org.junit.Ignore;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.shell.Input;
 import org.springframework.shell.Shell;
 import org.springframework.test.annotation.DirtiesContext;
 import ru.otus.krivonos.library.service.OutputService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -42,7 +44,20 @@ class ShellControllerTest {
 	@DirtiesContext(methodMode = DirtiesContext.MethodMode.BEFORE_METHOD)
 	@Test
 	void shouldPrintAllGenres() throws Exception {
-		String res = (String) shell.evaluate(() -> "all genres");
+		String res = (String) shell.evaluate(new Input() {
+			@Override
+			public String rawText() {
+				return "all genres";
+			}
+
+			@Override
+			public List<String> words() {
+				List<String> l = new ArrayList<>();
+				l.add("all");
+				l.add("genres");
+				return l;
+			}
+		});
 
 		assertThat(res).isEqualTo("");
 		verify(outputService, times(4)).printText(any());
@@ -54,38 +69,110 @@ class ShellControllerTest {
 
 	@Test
 	void shouldPrintBookByID() throws Exception {
-		String res = (String) shell.evaluate(() -> "book 1");
+		String res = (String) shell.evaluate(new Input() {
+			@Override
+			public String rawText() {
+				return "book 1";
+			}
+
+			@Override
+			public List<String> words() {
+				List<String> l = new ArrayList<>();
+				l.add("book");
+				l.add("1");
+				return l;
+			}
+		});
 
 		assertThat(res).isEqualTo("");
 		verify(outputService, times(1)).printText("Книга{id=1, название='Повести Белкина (сборник)', автор='Александр Пушкин', жанр='Литература 19 века'}");
 	}
 
-	@Disabled(value = "ошибка из-за наличия пробела в нвазнии литературного жанра")
 	@Test
 	void shouldReturnSuccessUpdateBookMessage() {
-		String res = (String) shell.evaluate(() -> "update book 1 новое_название_книги новый_автор 'Русская классика'");
+		String res = (String) shell.evaluate(new Input() {
+			@Override
+			public String rawText() {
+				return "update book 1 новое_название_книги новый_автор Русская классика";
+			}
+
+			@Override
+			public List<String> words() {
+				List<String> l = new ArrayList<>();
+				l.add("update");
+				l.add("book");
+				l.add("1");
+				l.add("новое_название_книги");
+				l.add("новый_автор");
+				l.add("Русская классика");
+				return l;
+			}
+		});
 
 		assertThat(res).isEqualTo("книга успешно обновлена");
 	}
 
-	@Disabled(value = "ошибка из-за наличия пробела в нвазнии литературного жанра")
 	@Test
 	void shouldReturnSuccessSaveBookMessage() {
-		String res = (String) shell.evaluate(() -> "save book название_книги автор_книги 'Русская классика'");
+		String res = (String) shell.evaluate(new Input() {
+			@Override
+			public String rawText() {
+				return "save book название_книги автор_книги Русская классика";
+			}
+
+			@Override
+			public List<String> words() {
+				List<String> l = new ArrayList<>();
+				l.add("save");
+				l.add("book");
+				l.add("название_книги");
+				l.add("автор_книги");
+				l.add("Русская классика");
+				return l;
+			}
+		});
 
 		assertThat(res).isEqualTo("книга успешно сохранена");
 	}
 
 	@Test
 	void shouldReturnSuccessBookDeleteMessage() {
-		String res = (String) shell.evaluate(() -> "delete book 2");
+		String res = (String) shell.evaluate(new Input() {
+			@Override
+			public String rawText() {
+				return "delete book 2";
+			}
+
+			@Override
+			public List<String> words() {
+				List<String> l = new ArrayList<>();
+				l.add("delete");
+				l.add("book");
+				l.add("2");
+				return l;
+			}
+		});
 
 		assertThat(res).isEqualTo("книга успешно удалена");
 	}
 
 	@Test
 	void saveReturnSuccessSaveGenreMessage() {
-		String res = (String) shell.evaluate(() -> "save genre новый_литературный_жанр");
+		String res = (String) shell.evaluate(new Input() {
+			@Override
+			public String rawText() {
+				return "save genre новый_литературный_жанр";
+			}
+
+			@Override
+			public List<String> words() {
+				List<String> l = new ArrayList<>();
+				l.add("save");
+				l.add("genre");
+				l.add("новый_литературный_жанр");
+				return l;
+			}
+		});
 
 		assertThat(res).isEqualTo("литературный жанр 'новый_литературный_жанр' успешно сохранен");
 	}
