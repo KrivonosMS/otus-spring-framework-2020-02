ef58808573e7616589a016abd237be3351f3c3b3
KrivonosMS <MS.Krivonos@gamil.com>
2020-07-07 22:37:40 +0300

    feat: реализзовано дз по docker

---
+++ homework-17-docker/library-app/src/main/resources/schema.sql
@@ -0,0 +1,53 @@
+DROP TABLE IF EXISTS comment;
+DROP TABLE IF EXISTS book;
+DROP TABLE IF EXISTS author;
+DROP TABLE IF EXISTS genre;
+DROP TABLE IF EXISTS user_role;
+DROP TABLE IF EXISTS app_user;
+DROP TABLE IF EXISTS role;
+
+CREATE TABLE genre (
+  id    BIGSERIAL PRIMARY KEY NOT NULL,
+  type  VARCHAR(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE author (
+  id   BIGSERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE book (
+  id            BIGSERIAL PRIMARY KEY NOT NULL,
+  title         VARCHAR(255) NOT NULL,
+  author_id     INTEGER,
+  genre_id      INTEGER,
+  FOREIGN KEY (author_id) REFERENCES author(id),
+  FOREIGN KEY (genre_id) REFERENCES genre(id)
+);
+
+CREATE TABLE comment (
+  id                BIGSERIAL PRIMARY KEY NOT NULL,
+  comment           VARCHAR(4000) NOT NULL,
+  creation_date     TIMESTAMP NOT NULL,
+  book_id           INTEGER,
+  FOREIGN KEY (book_id) REFERENCES book(id)
+);
+
+CREATE TABLE app_user (
+  id                BIGSERIAL PRIMARY KEY NOT NULL,
+  login             VARCHAR(4000) NOT NULL,
+  password          VARCHAR(4000) NOT NULL
+);
+
+CREATE TABLE role (
+  id                BIGSERIAL PRIMARY KEY NOT NULL,
+  name              VARCHAR(4000) NOT NULL
+);
+
+CREATE TABLE user_role (
+  id                BIGSERIAL PRIMARY KEY NOT NULL,
+  user_id           INTEGER,
+  role_id           INTEGER,
+  FOREIGN KEY (user_id) REFERENCES app_user(id),
+  FOREIGN KEY (role_id) REFERENCES role(id)
+);
